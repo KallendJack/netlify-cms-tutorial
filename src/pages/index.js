98144583f7fccd0da2d4eb3684c792692f489445
Jack Kallend <Kallendo96@live.co.uk>
2020-03-09 09:47:44 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <form name="contact" netlify>
+    <p>
+      <label>
+        Name <input type="text" name="name" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Email <input type="email" name="email" />
+      </label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+)
